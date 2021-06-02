2f3ddc1fd98cb5a4f02598049514c1b6cdeaba8f
Brett Andrews <brettandrews3@gmail.com>
2021-06-02 17:00:39 -0500

    Renamed random_choice var to computer_choice for better readability; added conditions for tie round.

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -2,25 +2,31 @@
 
 import random
 
+# This variable will hold the winner info later in the code.
+winner = ''
+
 print('Welcome to Rock, Paper, Scissors.\n')
-random_choice = random.randint(0,2)
+computer_choice = random.randint(0,2) # Keeps the computer to just 3 choices
 
 # Alternate randomizer:
 """
 choices = ['rock', 'paper', 'scissors']
-random_choice = random.choice(choices)
+computer_choice = random.choice(choices)
 """
 
 # The computer makes its choice:
-if random_choice == 0:
-    random_choice = 'rock'
-elif random_choice == 1:
-    random_choice = 'paper'
+if computer_choice == 0:
+    computer_choice = 'rock'
+elif computer_choice == 1:
+    computer_choice = 'paper'
 else:
-    random_choice = 'scissors'
+    computer_choice = 'scissors'
 
 # Next, the user makes their choice:
 user_choice = input('What do you choose: rock, paper, or scissors?\n')
 
 # Print the choices...
-print('You chose', user_choice, 'and the computer chose', random_choice + '.')
+print('You chose', user_choice, 'and the computer chose', computer_choice + '.\n')
+
+if user_choice == computer_choice:
+    winner = 'Tie'
